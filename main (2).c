7f52aceaa031da326844a4b5fa9b21d49719d262
221710302050 <59170968+221710302050@users.noreply.github.com>
2019-12-31 10:14:08 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#include<stdio.h>
+void main()
+{
+    int a,c,b;
+    b=scanf("%d%d",&a,&c);
+    
+    printf("\n%d",b);
+
+    
+}
